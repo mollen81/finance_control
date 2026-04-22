8970318ff9054da0e7ea6f631a16e4a423c30995
mollen <stepanpanenko@gmail.com>
2026-04-22 17:29:13 +0300

    t-api TinkoffConfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.model.internal.core.ModelNodes.all
+
 group = "org.mollen"
 version = "0.1"
 
@@ -17,5 +19,33 @@ dependencies {
     implementation("com.google.protobuf:protobuf-java:4.30.2")
     implementation("com.google.protobuf:protobuf-java-util:4.30.2")
 
+    // JavaX
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.24.0"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.62.2"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                create("grpc")
+            }
+        }
+    }
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("src/main/proto")
+        }
+    }
 }
 
---
+++ src/main/java/org/mollen/config/TinkoffConfig.java
@@ -0,0 +1,19 @@
+package org.mollen.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.tinkoff.piapi.core.InvestApi;
+
+@Configuration
+public class TinkoffConfig {
+
+    @Value("${t-api_sandbox-token}")
+    private String sandboxToken;
+
+    @Bean
+    public InvestApi investApi() {
+        // Sandbox
+        return InvestApi.create(sandboxToken);
+    }
+}
