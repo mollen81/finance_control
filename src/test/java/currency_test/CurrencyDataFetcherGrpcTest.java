be11f9e101572327883c99bcc6c95611fdc37b97
mollen <stepanpanenko@gmail.com>
2026-04-23 19:00:10 +0300

    t-api Currency gRPC service test fix Tested-by: CurrencyDataFetcherGrpcTest - completed

--- src/test/java/currency_test/CurrencyDataFetcherGrpcTest.java
+++ src/test/java/currency_test/CurrencyDataFetcherGrpcTest.java
@@ -1,6 +1,8 @@
 package currency_test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.project.grpc.Currency;
 import org.project.grpc.GetCurrencyByIdRequest;
 import org.project.grpc.GetCurrencyByIdResponse;
 import org.project.grpc.IdType;
@@ -8,12 +10,37 @@ import org.project.grpc.IdType;
 public class CurrencyDataFetcherGrpcTest extends InitTest {
     @Test
     public void getCurrencyByIdTest() {
+
         GetCurrencyByIdResponse response = blockingStub.getCurrencyById(
                 GetCurrencyByIdRequest.newBuilder()
                         .setIdType(IdType.ID_TYPE_TICKER)
                         .setClassCode("CETS")
                         .setId("AMDRUB_TOM")
-                        .build()
+                        .build());
+
+        Currency expected = Currency.newBuilder()
+                .setFigi("BBG0013J7V24")
+                .setTicker("AMDRUB_TOM")
+                .setClassCode("CETS")
+                .setIsin("")
+                .setLot(0)
+                .setCurrency("rub")
+                .setName("Армянский драм")
+                .setExchange("FX")
+                .setCountryOfRisk("")
+                .setIsoCurrencyName("amd")
+                .setUid("86d28f8d-379d-422f-b01d-c32ee0b97501")
+                .setPositionUid("4515b482-71f0-4193-aa0a-b91efdab7062")
+                .setBuyAvailable(false)
+                .setSellAvailable(false)
+                .setApiTradeAvailable(false)
+                .setForIis(false)
+                .setWeekendFlag(false)
+                .build();
+
+        Assertions.assertEquals(
+                response.getCurrency().toBuilder().toString(),
+                expected.toBuilder().toString()
         );
     }
 }
