ccadbf39a5575a4380f5b56dcbd00ea4acf84d37
mollen <stepanpanenko@gmail.com>
2026-04-25 17:37:25 +0300

    t-api: init for Bond service tests

--- t-api/src/test/java/bond_test/InitTest.java
+++ t-api/src/test/java/bond_test/InitTest.java
@@ -1,4 +1,23 @@
 package bond_test;
 
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.BeforeAll;
+import org.project.grpc.BondDataFetcherGrpc;
+
 public class InitTest {
+    protected static BondDataFetcherGrpc.BondDataFetcherBlockingStub blockingStub;
+    protected static Channel channel;
+
+    // Communication channel
+    @BeforeAll
+    public static void init() {
+         channel = ManagedChannelBuilder
+                .forAddress("localhost", 9090)
+                 .usePlaintext()
+                .build();
+
+         blockingStub = BondDataFetcherGrpc.newBlockingStub(channel);
+    }
+
 }
